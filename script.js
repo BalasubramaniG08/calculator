114cc510255bb5589948049a47db2626e40523c2
BalasubramaniG08 <balamech019@gmail.com>
2025-06-06 23:03:15 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+function calculate() 
+
+{
+    let num1 = parseFloat(document.getElementById("num1").value);
+    let num2 = parseFloat(document.getElementById("num2").value);
+    let operator = document.getElementById("operator").value;
+
+let result;
+
+   
+     if (operator == "+") {
+       result = num1 + num2;
+     } else if (operator == "-") {
+       result = num1 - num2;
+     } else if (operator == "*") {
+       result = num1 * num2;
+     } else if (operator == "/") {
+       result = num1 / num2;
+     } 
+
+   document.getElementById("result").value=result
+
+}
